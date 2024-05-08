68fb68dfdc1e12f7455e9cfba0b95061c7e50e36
Karim Samaha <kareem.mahmoud1237@gmail.com>
2024-05-08 18:59:37 +0300

    CSS_JEST_MODIFICATION

--- src/components/home/tests/heroSection.test.js
+++ src/components/home/tests/heroSection.test.js
@@ -1,14 +1,17 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import HeroSection from "../HeroSection";
 import { HeroData } from "../../../data/Data";
-
+import styles from '../home.module.css'
 const checkButtonBehavior = (index) => {
     return it(`Check Hero Button Number ${index + 1} Is Working`, () => {
         render(<HeroSection />);
         const button = screen.getByTestId(`bullet-${index}`)
         fireEvent.click(button)
-        const textElement = screen.getByText(HeroData[index].title)
-        expect(textElement).toBeInTheDocument();
+        const titleElement = screen.getByText(HeroData[index].title)
+        const subTitle = screen.getByText(HeroData[index].body)
+        expect(titleElement).toBeInTheDocument();
+        expect(subTitle).toBeInTheDocument();
+
     });
 }
 describe("Hero Section", () => {
